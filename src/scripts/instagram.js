11988955cb7b27122ff69d6091068b94bf095fd9
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-08-19 01:23:08 +0530

    Fix tag

--- src/scripts/instagram.js
+++ src/scripts/instagram.js
@@ -1,7 +1,7 @@
 import fetch from "./fetch";
 
 const CLIENT_ID = "a93dfcee57b64124adc5fbc10181a7d0";
-const TAG = "a93dfcee57b64124adc5fbc10181a7d0";
+const TAG = "IAmAyana";
 
 export default function instagram(cb) {
     fetch(`https://api.instagram.com/v1/tags/${TAG}/media/recent?client_id=${CLIENT_ID}`, function(err, res) {
