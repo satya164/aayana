1a343e2cdf5950a8e06c5cc30393768c1f9fc67a
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-08-19 20:25:22 +0530

    Don't wait till document ready to load instagram photos

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,53 +1,53 @@
 import instagram from "./instagram";
 
-function loadPhotos() {
-    instagram((err, res) => {
-        let stream = document.getElementById("instagram-stream");
+function loadPhotos(err, res) {
+    let stream = document.getElementById("instagram-stream");
 
-        if (err) {
-            stream.textContent = "An error occurred while loading photos :(";
+    if (err) {
+        stream.textContent = "An error occurred while loading photos :(";
 
-            return;
-        }
+        return;
+    }
 
-        if (res.data.length === 0) {
-            stream.textContent = "No photos found :(";
+    if (res.data.length === 0) {
+        stream.textContent = "No photos found :(";
 
-            return;
-        }
+        return;
+    }
 
-        let fragment = document.createDocumentFragment();
+    let fragment = document.createDocumentFragment();
 
-        res.data.forEach(p => {
-            let div = document.createElement("div"),
-                link = document.createElement("a"),
-                image = document.createElement("img");
+    res.data.forEach(p => {
+        let div = document.createElement("div"),
+            link = document.createElement("a"),
+            image = document.createElement("img");
 
-            div.className = "col small-12 medium-6 large-4";
+        div.className = "col small-12 medium-6 large-4";
 
-            link.className = "photo";
-            link.target = "_blank";
-            link.href = p.link;
+        link.className = "photo";
+        link.target = "_blank";
+        link.href = p.link;
 
-            image.src = p.images.standard_resolution.url;
+        image.src = p.images.standard_resolution.url;
 
-            link.appendChild(image);
-            div.appendChild(link);
+        link.appendChild(image);
+        div.appendChild(link);
 
-            fragment.appendChild(div);
-        });
-
-        stream.innerHTML = "";
-        stream.appendChild(fragment);
+        fragment.appendChild(div);
     });
+
+    stream.innerHTML = "";
+    stream.appendChild(fragment);
 }
 
-if (document.readyState === "complete") {
-    loadPhotos();
-} else {
-    document.onreadystatechange = function () {
-        if (document.readyState === "complete") {
-            loadPhotos();
-        }
-    };
-}
+instagram((err, res) => {
+    if (document.readyState === "complete") {
+        loadPhotos(err, res);
+    } else {
+        document.onreadystatechange = () => {
+            if (document.readyState === "complete") {
+                loadPhotos(err, res);
+            }
+        };
+    }
+});
